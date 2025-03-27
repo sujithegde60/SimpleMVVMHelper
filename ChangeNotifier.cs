e7d7f923f8f304d0ea7a1f3667bbf6b71df2df2d
Sujit Hegde <sujithegde60@gmail.com>
2025-03-27 12:53:27 +0530

    added propertychanging event handler

--- ChangeNotifier.cs
+++ ChangeNotifier.cs
@@ -7,9 +7,16 @@ using System.Threading.Tasks;
 
 namespace SimpleMVVMHelper
 {
-    public class ChangeNotifier : INotifyPropertyChanged
+    public class ChangeNotifier : INotifyPropertyChanged, INotifyPropertyChanging
     {
         public event PropertyChangedEventHandler? PropertyChanged;
+        public event PropertyChangingEventHandler? PropertyChanging;
+
+        protected void OnPropertyChanging(string propertyName)
+        {
+            PropertyChanging?.Invoke(this, new PropertyChangingEventArgs(propertyName));
+        }
+
         protected void OnPropertyChanged(string propertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
